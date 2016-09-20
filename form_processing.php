19912489a01be9232b1c336ec1018cb1d9f1d4ac
Leah Chan <lec533@g.harvard.edu>
2016-09-19 22:57:17 -0400

    added logic for password generator

--- form_processing.php
+++ form_processing.php
@@ -1,25 +1,49 @@
 <?php
 
-$inputNumWords = 0;
+$word_lib = array("Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Nulla", "accumsan", "tortor", "quis", "dignissim", "interdum", "Pellentesque", "in", "felis", "aliquam", "accumsan", "libero", "quis", "congue", "magna", "Sed", "rutrum", "nunc", "quis", "lectus", "egestas", "vestibulum", "Vestibulum", "ultricies", "orci", "et", "metus");
+
+$special_chars_lib = array("@", "#", "$", "%", "&", "*");
+
+$error_msg_num_GT5 = "You have entered a number greater than 5. Please enter a number between 0 and 5.";
+
+$generated_pw = "";
+$index = 0;
 
 if (isset($_POST['numWords'])) {
     $inputNumWords = $_POST['numWords'];
-  
+    $inputNumWords *= 1;
+    
+    /*
+    if($inputNumWords is null){
+        //generate error msg
+    }
+    elseif($inputNumWords == 0){
+        //generate error msg
+    }
+    elseif($inputNumWords > 5){
+       //generate error msg
+    }
+    */
 };
-
 
 if (isset( $_POST['add_special_char'])) {
     $inputAddSpecialChars =  $_POST['add_special_char'];
-    echo 'add special chars!';
 };
 
 if (isset( $_POST['add_numbers'])) {
     $inputAddNumbers =  $_POST['add_numbers'];
-    echo 'add #!';
 };
+
+//  generate a random number to determine which word to pick from our
+//  word library and concatenate values to $generated_pw
+for ($i = 1; $i <= $inputNumWords; $i++) {
+    $index = rand(0, 34);
+    $generated_pw = $generated_pw . $word_lib[$index] .  " - ";
+}
 
 var_dump($inputNumWords);
 var_dump($inputAddSpecialChars);
 var_dump($inputAddNumbers);
+var_dump($generated_pw);
 
 ?>
