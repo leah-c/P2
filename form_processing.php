2b1abea2ba335bb129e7f0afcaefc0b8b43a2ab1
Leah Chan <lec533@g.harvard.edu>
2016-09-21 21:57:41 -0400

    updated form validation to display correct msgs

--- form_processing.php
+++ form_processing.php
@@ -29,9 +29,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     //  check to see if input is empty, non-numeric
     //  or out of range
     //================================================
+    var_dump($_POST['numWords']);
+    var_dump(empty($_POST['numWords']));    
+    var_dump(is_null($_POST['numWords']));    
+    var_dump(intval($_POST['numWords'])); 
+    var_dump(isset($_POST['numWords']) && $_POST['numWords'] != '');
     
    // added intval() check for 0 since that is being read as an empty string
-   if (empty($_POST['numWords']) && !(intval($_POST['numWords'] == 0))){
+    if ((isset($_POST['numWords'])) && ($_POST['numWords'] == "")){
+
         $display_error_msg =  $empty_value_error;
     }
     
